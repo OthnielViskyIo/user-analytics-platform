1be9eff7c181483fea4cc85a0cf61051e97b53b7
Othniel Visky <othniel.visky@vodafoneziggo.com>
2026-06-16 16:53:09 +0200

    feat: add secret content

--- apps/tester-ui/app/page.tsx
+++ apps/tester-ui/app/page.tsx
@@ -9,30 +9,38 @@ import rock from '@/assets/rock.png'
 
 export default function Home() {
   return (
-    <Card>
-      <h1 className="text-4xl font-bold mb-8">Home</h1>
-      <div className="grid grid-cols-2 gap-4">
-        <Link href="/biomes/desert">
-          <ImageWrapper>
-            <Image src={desert} alt="Desert" className="object-cover" fill />
-          </ImageWrapper>
-        </Link>
-        <Link href="/biomes/jungle">
-          <ImageWrapper>
-            <Image src={jungle} alt="Jungle" className="object-cover" fill />
-          </ImageWrapper>
-        </Link>
-        <Link href="/biomes/ocean">
-          <ImageWrapper>
-            <Image src={ocean} alt="Ocean" className="object-cover" fill />
-          </ImageWrapper>
-        </Link>
-        <Link href="/biomes/rock">
-          <ImageWrapper>
-            <Image src={rock} alt="Rock" className="object-cover" fill />
-          </ImageWrapper>
-        </Link>
-      </div>
-    </Card>
+    <div className="flex flex-col gap-8">
+      <Card>
+        <h1 className="text-4xl font-bold mb-8">Home</h1>
+        <div className="grid grid-cols-2 gap-4">
+          <Link href="/biomes/desert">
+            <ImageWrapper>
+              <Image src={desert} alt="Desert" className="object-cover" fill />
+            </ImageWrapper>
+          </Link>
+          <Link href="/biomes/jungle">
+            <ImageWrapper>
+              <Image src={jungle} alt="Jungle" className="object-cover" fill />
+            </ImageWrapper>
+          </Link>
+          <Link href="/biomes/ocean">
+            <ImageWrapper>
+              <Image src={ocean} alt="Ocean" className="object-cover" fill />
+            </ImageWrapper>
+          </Link>
+          <Link href="/biomes/rock">
+            <ImageWrapper>
+              <Image src={rock} alt="Rock" className="object-cover" fill />
+            </ImageWrapper>
+          </Link>
+        </div>
+      </Card>
+
+      <Card>
+        <div className="h-125 flex items-center justify-center">
+          <p className="text-lg text-zinc-600 dark:text-zinc-400">secret content</p>
+        </div>
+      </Card>
+    </div>
   )
 }
--- apps/tester-ui/components/Card.tsx
+++ apps/tester-ui/components/Card.tsx
@@ -2,7 +2,9 @@ import { PropsWithChildren } from 'react'
 
 export function Card({ children }: PropsWithChildren) {
   return (
-    <div className="bg-white dark:bg-zinc-900 border border-zinc-200 dark:border-zinc-800 rounded-lg shadow-sm p-8 min-h-100">
+    <div
+      className={`bg-white dark:bg-zinc-900 border border-zinc-200 dark:border-zinc-800 rounded-lg shadow-sm p-8 min-h-100`}
+    >
       {children}
     </div>
   )
