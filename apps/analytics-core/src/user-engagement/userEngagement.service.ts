802565aa7034e1431eaedf2903873471389ab584
Othniel Visky <othniel.visky@vodafoneziggo.com>
2026-06-19 14:14:00 +0200

    refactor: improve type safety in UserEngagementService

--- apps/analytics-core/src/user-engagement/userEngagement.service.ts
+++ apps/analytics-core/src/user-engagement/userEngagement.service.ts
@@ -1,15 +1,13 @@
 import { Injectable } from '@nestjs/common'
 import { InjectModel } from '@nestjs/mongoose'
 import { Model } from 'mongoose'
-import { differenceInSeconds } from 'date-fns'
 
 import { CaptureEvent } from '../capture-event/captureEvent.schema'
 
-type SomethingProperties = {
-  pathname: string
-  pageEnter: string
-  pageLeave: string
-  pageTransitionId: string
+type UserEngagementAggregationResult = {
+  _id: string
+  pageviews: number
+  timeOnPage: number
 }
 
 @Injectable()
@@ -19,34 +17,48 @@ export class UserEngagementService {
   ) {}
 
   async getUserEngagementStats() {
-    const captureEvent = await this.captureEventModel.aggregate<CaptureEvent>([
+    const results = await this.captureEventModel.aggregate<UserEngagementAggregationResult>([
       {
         $match: {
-          properties: { $exists: true, $type: 'object' },
-          $expr: { $gt: [{ $size: { $objectToArray: '$properties' } }, 0] },
+          'properties.pathname': { $exists: true, $ne: null },
+          'properties.pageEnter': { $exists: true },
+          'properties.pageLeave': { $exists: true, $ne: null },
+        },
+      },
+      {
+        $project: {
+          pathname: '$properties.pathname',
+          duration: {
+            $abs: {
+              $divide: [
+                {
+                  $subtract: [
+                    { $toDate: '$properties.pageLeave' },
+                    { $toDate: '$properties.pageEnter' },
+                  ],
+                },
+                1000,
+              ],
+            },
+          },
+        },
+      },
+      {
+        $group: {
+          _id: '$pathname',
+          pageviews: { $sum: 1 },
+          timeOnPage: { $sum: '$duration' },
         },
       },
     ])
-    const properties = captureEvent.map((event) => event.properties as SomethingProperties)
-    const filterOnPathname = properties.filter((props) => props.pathname)
 
-    const pageviews = filterOnPathname.reduce(
-      (viewsPerPage, property) => {
-        viewsPerPage[property.pathname] = (viewsPerPage[property.pathname] || 0) + 1
-        return viewsPerPage
-      },
-      {} as Record<string, number>,
-    )
+    const pageviews: Record<string, number> = {}
+    const timeOnPage: Record<string, number> = {}
 
-    const timeOnPage = filterOnPathname.reduce(
-      (timeSpentPerPage, currentProperty) => {
-        timeSpentPerPage[currentProperty.pathname] = Math.abs(
-          differenceInSeconds(currentProperty.pageEnter, currentProperty.pageLeave),
-        )
-        return timeSpentPerPage
-      },
-      {} as Record<string, number>,
-    )
+    for (const result of results) {
+      pageviews[result._id] = result.pageviews
+      timeOnPage[result._id] = result.timeOnPage
+    }
 
     return {
       key: 'userEngagement',
